da9ec111fae00a5dbd6a4ec48368e53cd86052bf
Daniel AMELINA <xdaniel2@hotmail.com>
2016-04-27 18:01:04 +0100

    Modif

--- src/WebService/Bundle/MoteurBundle/Controller/WebServiceController.php
+++ src/WebService/Bundle/MoteurBundle/Controller/WebServiceController.php
@@ -3,6 +3,9 @@
 namespace WebService\Bundle\MoteurBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+use WebService\Bundle\MoteurBundle\Entity\MobileUser;
 
 
 /*
@@ -14,8 +17,44 @@ class WebServiceController extends Controller
 {
 
 
-    public function registerAction($email, $phoneid, $gcmId)
+    /*
+     *
+     */
+    public function registerAction()
     {
-       
+
+        try{
+
+            $request = $this->getRequest();
+            $email = $request->get("email");
+            $phoneid = $request->get("phoneid");
+            $gcmId = $request->get("gcmId");
+
+            $mobileUser = new MobileUser();
+            $mobileUser->setDateEnregistrement(new \DateTime());
+            $mobileUser->setEmail($email);
+            $mobileUser->setPhoneid($phoneid);
+            $mobileUser->setGcmid($gcmId);
+
+            $jsonResponse = new JsonResponse();
+            $this->getDoctrine()->getManager()->persist($mobileUser);
+            $this->getDoctrine()->getManager()->persist();
+
+            $response = array("content" => "OK");
+
+        }
+        catch (Exception $ex)
+        {
+            $jsonResponse->setStatusCode("100");
+            $response = array("content" => "Error");
+        }
+        $jsonResponse->setContent($response);
+
+        return  $jsonResponse;
+
     }
+
+
+
+
 }
